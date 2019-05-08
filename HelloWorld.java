89b0ace5b65913d01584ac2d836c80fa5fd753cc
moshangsang24 <liuyifan731@163.com>
2019-05-08 12:10:47 +0800

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
    {  
        public static void main(String[] args)  
        {  
-           System.out.println("23World!");  
+           System.out.println("888git World!");  
        }   
    }  
